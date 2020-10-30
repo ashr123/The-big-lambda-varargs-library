1467b2e4d0db6d822407e9def0a6dfe562f40b95
Roy Ash <roy.ash456@gmail.com>
2020-10-30 14:57:51 +0200

    initiation commit

--- src/il/co/varargsLambdas/VarargsLambda0.java
+++ src/il/co/varargsLambdas/VarargsLambda0.java
@@ -3,5 +3,6 @@ package il.co.varargsLambdas;
 @FunctionalInterface
 public interface VarargsLambda0<T, R>
 {
+	@SuppressWarnings("unchecked")
 	R take(T... args);
 }
--- src/il/co/varargsLambdas/VarargsLambda1.java
+++ src/il/co/varargsLambdas/VarargsLambda1.java
@@ -3,5 +3,6 @@ package il.co.varargsLambdas;
 @FunctionalInterface
 public interface VarargsLambda1<T0, T1, R>
 {
+	@SuppressWarnings("unchecked")
 	R take(T0 arg1, T1... args);
 }
